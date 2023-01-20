9e63f474df3db48dfebe63f83e7734ab5e54b9cc
Nick Salvemini <salvemini.nick@gmail.com>
2023-01-20 15:36:07 -0500

    added JS functions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, render_template, redirect, flash, jsonify, request
+from flask import Flask, render_template, redirect, flash, jsonify, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,12 +9,16 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 boggle_game = Boggle()
 
-game_board = boggle_game.make_board()
+# if session.get('board'):
+#     game_board = session['board']
+# else:
+#     game_board = boggle_game.make_board()
+#     session['board'] = game_board
+
+game_board = boggle_game.make_board()   
 
 @app.route('/')
 def make_game():
-    
-    
     
     # guess = request.form['guess']
     # check_word = boggle_game.check_valid_word(game_board, guess)
@@ -23,16 +27,20 @@ def make_game():
     # flash(json_response)
     # flash(guess)
     
-
     return render_template('home.html', game_board=game_board)
 
-@app.route('/check_word', methods=['POST'])
+@app.route('/check_word')
 def request_check_word():
-    guess = request.form['guess']
-    # check_word = boggle_game.check_valid_word(game_board, guess)
+    guess = request.args.get('word')
+    check_word = boggle_game.check_valid_word(game_board, guess)
+    response = {'result': check_word}
+    json_response = jsonify(response)
+    return json_response
+
+    # guess = request.args.get('word')
     # response = {'result': check_word}
     # json_response = jsonify(response)
-    # flash(json_response)
+    # flash(guess)
     # return json_response
     # flash(guess)
     # return guess
--- static/app.js
+++ static/app.js
@@ -1,14 +1,46 @@
 let $guess = $('#guess').value()
-let $button = $('#submit')
+const $button = $('#submit')
+const $result = $('#result')
+
+let totalScore = 0
+const $score = totalScore
 
 async function checkWord() {
     let word = $guess;
-    const res = await axios.post('http://127.0.0.1:5000', word);
+    const res = await axios.get('http://127.0.0.1:5000/check_word', word);
 
     console.log(res)
 }
 
-$button.on('click', function (evt) {
+async function getJsonResponse() {
+    const res = await axios.get('http://127.0.0.1:5000/check_word')
+    console.log(res)
+    answerObj = JSON.parse(res)
+
+    return answerObj
+}
+
+function checkWordScore(word) {
+    score = word.length
+    return score
+}
+
+$button.on('click', async function (evt) {
     evt.preventDefault();
-    checkWord()
+    await checkWord();
+    answerObj = await getJsonResponse();
+
+    word = answerObj['result']
+
+    if (word === 'not-word')
+        $result.val(`${word} is not a word`)
+    else if (word === 'not-on-board')
+        $result.val(`${word} is not on the current board`)
+    else
+        wordScore = checkWordScore(word)
+    $result.val(`${word} was worth ${wordScore} points!`)
+    totalScore += wordScore
 })
+
+
+
